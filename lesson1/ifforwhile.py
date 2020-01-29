56480f555dfa3fada49aff7c69ae984279066324
mamba <qct731IAwe9>
2020-01-29 22:37:42 +0300

    if for while

---
+++ lesson1/ifforwhile.py
@@ -0,0 +1,17 @@
+decision = True
+
+
+if decision:
+    print('decisoin is true')
+else:
+    print('decision is false')
+
+
+for max in range(1, 5):
+    print(max)
+
+mycounter = 0
+
+while mylist < 10:
+    mycounter = mycounter + 1
+    print(mycounter)
